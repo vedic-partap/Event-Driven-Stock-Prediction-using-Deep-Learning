d05c30a83e06bf2a22ad465f3277191416faeb59
walragatver <toshalagrawal38@gmail.com>
2018-12-23 18:49:59 +0530

    Made Word Embeddings for vocabulary using stanford Glove Vectors.

--- genFeatureMatrix.py
+++ genFeatureMatrix.py
@@ -19,21 +19,14 @@ def unify_word(word): # went -> go, apples -> apple, BIG -> big
     # except: pass
     return word.lower()
 
-def readGlove(we_file, w2i_file, concat=True):
-    npz = np.load(we_file)
-    W1 = npz['arr_0']
-    W2 = npz['arr_1']
-    with open(w2i_file) as f:
-        word2idx = json.load(f)
-
-    V = len(word2idx)
-    if concat:
-        We = np.hstack([W1, W2.T])
-        print("We.shape:", We.shape)
-        assert(V == We.shape[0])
-    else:
-        We = (W1 + W2.T) / 2
-    return We
+def readGlove(we_file):
+    wordVec = np.zeros([0,100])
+    with open(we_file) as file:
+        for line in file:
+            line = line.strip().split()
+            line = list(map(float,line))
+            wordVec = np.vstack((wordVec,np.array(line).flatten()))
+    return wordVec
 
 def padding(sentencesVec, keepNum):
     shape = sentencesVec.shape[0]
@@ -47,36 +40,47 @@ def gen_FeatureMatrix(wordEmbedding, word2idx, priceDt, max_words=60, mtype="tes
     # step 2, build feature matrix for training data
     loc = './input/'
     input_files = [f for f in os.listdir(loc) if f.startswith('news_reuters.csv')]
+    #print(input_files)
     current_idx = 2
     dp = {} # only consider one news for a company everyday
     cnt = 0
-    testDates = dateGenerator(100)
+    #testDates = dateGenerator(100)
     shape = wordEmbedding.shape[1]
+    print(shape)
     features = np.zeros([0, max_words * shape])
+    #print(features.shape)
     labels = []
     for file in input_files:
-        for line in open(loc + file):
-            line = line.strip().split(',')
-            if len(line) != 5: continue
-            ticker, name, day, headline, body = line
-            if ticker not in priceDt: continue # skip if no corresponding company found
-            if day not in priceDt[ticker]: continue # skip if no corresponding date found
-            cnt += 1
-            print(cnt)
-            if mtype == "test" and day not in testDates: continue
-            if mtype == "train" and day in testDates: continue
-            # 2.1 tokenize sentense, check if the word belongs to the top words, unify the format of words
-            tokens = nltk.word_tokenize(headline) + nltk.word_tokenize(body)
-            tokens = [unify_word(t) for t in tokens]
-            #tokens = [t for t in tokens if t in stopWords]
-            #tokens = [t for t in tokens if t in topWords]
-            # 2.2 create word2idx/idx2word list, and a list to count the occurence of words
-            sentencesVec = np.zeros([shape, 0])
-            for t in tokens:
-                if t not in word2idx: continue
-                sentencesVec = np.hstack((sentencesVec, np.matrix(wordEmbedding[word2idx[t]]).T))
-            features = np.vstack((features, padding(sentencesVec, max_words)))
-            labels.append(round(priceDt[ticker][day], 6))
+        count = 0 # Not more than 50k news
+        with open(loc+file) as f:
+            if(mtype == 'test'):
+                f.seek(140000,0)              # seek to end of file; f.seek(0, 2) is legal
+            for line in f:
+                if mtype == 'test' and count == 1000: break
+                if mtype == 'train' and count  == 50000: break
+                line = line.strip().split(',')
+                if len(line) != 6: continue
+                ticker, name, day, headline, body ,newsType= line
+                #print("Toshal")
+                if ticker not in priceDt: continue # skip if no corresponding company found
+                if day not in priceDt[ticker]: continue # skip if no corresponding date found
+                cnt += 1
+                print(cnt)
+                #if mtype == "test" and day not in testDates: continue
+                #if mtype == "train" and day in testDates: continue
+                # 2.1 tokenize sentense, check if the word belongs to the top words, unify the format of words
+                tokens = nltk.word_tokenize(headline) + nltk.word_tokenize(body)
+                tokens = [unify_word(t) for t in tokens]
+                #tokens = [t for t in tokens if t in stopWords]
+                #tokens = [t for t in tokens if t in topWords]
+                # 2.2 create word2idx/idx2word list, and a list to count the occurence of words
+                sentencesVec = np.zeros([shape, 0])
+                for t in tokens:
+                    if t not in word2idx: continue
+                    sentencesVec = np.hstack((sentencesVec, np.matrix(wordEmbedding[word2idx[t]]).T))
+                features = np.vstack((features, padding(sentencesVec, max_words)))
+                count+=1 # increment news count
+                labels.append(round(priceDt[ticker][day], 6))
     features = np.array(features)
     labels = np.matrix(labels)
     featureMatrix = np.concatenate((features, labels.T), axis=1)
@@ -84,22 +88,20 @@ def gen_FeatureMatrix(wordEmbedding, word2idx, priceDt, max_words=60, mtype="tes
     np.savetxt(fileName, featureMatrix, fmt="%s")
 
 def build(wordEmbedding, w2i_file, max_words=60):
-    with open('./input/stockPrices.json') as data_file:    
+    with open('./input/stockPrices.json') as data_file:
         priceDt = json.load(data_file)
-    with open(w2i_file) as data_file:    
+    with open(w2i_file) as data_file:
         word2idx = json.load(data_file)
-    
+
     gen_FeatureMatrix(wordEmbedding, word2idx, priceDt, max_words, "train")
     gen_FeatureMatrix(wordEmbedding, word2idx, priceDt, max_words, "test")
-    
-                
-                    
+
 def main(we, w2i_file):
-    wordEmbedding = readGlove(we, w2i_file)
+    wordEmbedding = readGlove(we)
     build(wordEmbedding, w2i_file, 30)
 
 
 if __name__ == "__main__":
-    we = './input/glove_model_50.npz'
+    we = './input/wordEmbeddingsVocab.csv'
     w2i_file = "./input/word2idx.json"
     main(we, w2i_file)
